365aeed3d2edbebe989fa3555bc5692c917f56a2
DoodleCoder <gaurav290497@gmail.com>
2018-05-06 01:54:56 +0530

    url changes done

--- LizApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# -*- coding: utf-8 -*-
-from LizApp.models import *
-
-admin.site.register(Diary)
-admin.site.register(Wallet)
-admin.site.register(Transaction)
-admin.site.register(Entry)
--- LizApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LizappConfig(AppConfig):
-    name = 'LizApp'
