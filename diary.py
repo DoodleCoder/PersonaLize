412ac065aa88505f0a12040d9e38876006b89613
DoodleCoder <gaurav290497@gmail.com>
2018-05-07 00:17:28 +0530

    search added

--- diary.py
+++ diary.py
@@ -1,9 +1,9 @@
-import datetime as dt
+from datetime import datetime as dt
 
 f1 = open('diary','w+')
 f1.write("MY DIARY TRYOUT #1")
-dNow = dt.datetime.now().day
-mNow = dt.datetime.now().month
+dNow = dt.now().day
+mNow = dt.now().month
 def switch(mNow):
 	return {
 		'1':'January',
@@ -20,6 +20,6 @@ def switch(mNow):
 		'12':'December',
 	}[mNow]
 mNowNew = switch(mNow)
-yNow = dt.datetime.now().year
+yNow = dt.now().year
 f1.write("\t"+str(dNow)+"-"+str(mNowNew)+"-"+str(yNow))
 f1.close()
